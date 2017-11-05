92fcbc7ccc1a5ee06dd53eebcc0aa602f7eaa1ec
Ravin Kumar <ravinsdrive@gmail.com>
2017-11-05 14:42:59 -0800

    Add submission generator

--- spookyauthor/models/predict_model.py
+++ spookyauthor/models/predict_model.py
@@ -1,4 +1,27 @@
 """
 All code for predictive models goes here
 """
+import pandas as pd
+
+
+def submission_generator(test_df, pipeline, filename):
+    """Takes test dataframe and object that implements transform/predict methods
+    and writes of predictions for submission to Kaggle
+    
+    Returns
+    -------
+    Dataframe of predictions
+    """
+    
+    ids=test_df["id"]
+    text = df["text"]
+
+    preds = pipeline.predict_proba(text)
+
+    df = pd.DataFrame(preds)
+    df["id"] = ids
+    df = df.set_index("id")
+
+    df.to_csv(filename)
+    return df
 
