1395add701e28a6daaed96d5ed1c6af67550be6d
Ravin Kumar <ravinsdrive@gmail.com>
2018-06-17 19:21:48 -0700

    Add output for Kaggle submission

--- submissions/6_keras_embedding/keras_embedding.py
+++ submissions/6_keras_embedding/keras_embedding.py
@@ -20,15 +20,15 @@ train, test = make_dataset.load_raw_data()
 
 # Preprocess  data
 from spookyauthor.data.make_dataset import Preprocessing
-id, text, authors = Preprocessing.split_cols(train)
+ids, text, authors = Preprocessing.split_cols(train)
 
 from sklearn.preprocessing import LabelBinarizer
 l = LabelBinarizer()
 labels = l.fit_transform(authors)
-print(labels[:50])
 
 # fix random seed for reproducibility and set max words
-num_words = 3
+num_words = 10000
+num_input_length = 500
 
 # Preprocess text
 from keras.preprocessing.text import Tokenizer
@@ -37,7 +37,7 @@ from keras.preprocessing.sequence import pad_sequences
 tokenizer = Tokenizer(num_words=num_words)
 tokenizer.fit_on_texts(text)
 word_sequences = tokenizer.texts_to_sequences(text)
-padded_word_sequences = pad_sequences(word_sequences, maxlen=num_words)
+padded_word_sequences = pad_sequences(word_sequences, maxlen=num_input_length)
 
 
 # Make model
@@ -47,15 +47,44 @@ from keras.layers import LSTM
 
 # 19579 rows
 # 3 words
-
+"""
+Embedding needs the following parameters
+input_dim = Number of vocab tokenized + 1
+input_length = length of input arrays
+https://stackoverflow.com/questions/40644372/keras-the-difference-input-dim-and-input-length-in-lstm
+"""
 
 model = Sequential()
-model.add(Embedding(input_dim=4, output_dim=128, input_length=num_words))
+model.add(Embedding(input_dim=num_words+1, output_dim=128, input_length=num_input_length))
 model.add(LSTM(128))
 model.add(Dense(3, activation='softmax'))
 model.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 
-model.fit(padded_word_sequences, labels, batch_size=100, epochs=10, verbose=10)
+model.fit(padded_word_sequences, labels, batch_size=500, epochs=30, validation_split=.1, shuffle=True, verbose=10)
 
+
+
+# Get Max Prediction from each class
+# subset = predictions[:10]
+# print(predictions[:10])
+# predicted_author = np.zeros(labels.shape)
+# predicted_author[np.arange(predicted_author.shape[0]), np.argmax(predictions, axis=1)] = 1
+# print(predicted_author[:10])
+# print(True)
+
+# Make Predictions
+import pandas as pd
+test_ids, test_text, authors = Preprocessing.split_cols(test)
+
+word_sequences = tokenizer.texts_to_sequences(test_text)
+padded_word_sequences = pad_sequences(word_sequences, maxlen=num_input_length)
+
+predictions = model.predict(padded_word_sequences)
+
+df = pd.DataFrame(predictions, columns=["EAP", "HPL", "MWS"])
+df["id"] = test_ids
+df = df.set_index("id")
+
+df.to_csv("KerasEmbeddings.csv")
--- tests/test_scorer.py
+++ tests/test_scorer.py
@@ -3,6 +3,7 @@ Ensure that scorer works as expected
 """
 
 from sklearn.metrics import log_loss
+import numpy as np
 import pytest
 
 
@@ -14,3 +15,16 @@ def test_scorer():
 
     assert log_loss(y_true, y_pred, labels=columns) == pytest.approx(0)
 
+
+def test_numpy_softmax_conversion():
+    """Check that getting max prediction is working"""
+    scores = np.array([[100, 0.3231506,  0.2042883],
+                      [0.40807605, 200,  0.31776983],
+                      [300, 0.28616235, 0.23457035]])
+
+    one_hot_score = np.zeros(scores.shape)
+    one_hot_score[np.arange(3), np.argmax(scores, axis=1)] = 1
+    print(one_hot_score)
+    assert (one_hot_score == np.array([[1, 0, 0],
+                                      [0, 1, 0],
+                                      [1, 0, 0]])).all()
